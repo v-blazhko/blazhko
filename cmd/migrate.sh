5f0c57d08664fe98138da23a0a63eb3707798474
v-blazhko <veronica@blazhko.tech>
2020-11-29 21:37:35 +0200

    Enable migrations

--- cmd/migrate.sh
+++ cmd/migrate.sh
@@ -2,6 +2,8 @@
 
 mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD"
 
+echo 'Starting migrations...'
+
 for i in {30..0}; do
 			if echo 'SELECT 1' | mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" &> /dev/null; then
 				break
@@ -10,7 +12,7 @@ for i in {30..0}; do
 			sleep 1
 		done
 
-for f in /docker-entrypoint-migrations.d/*.up.sql; do
+for f in /docker-entrypoint-migrations.d/*; do
 			case "$f" in
 				*.sql)    echo "$0: running $f"; mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < "$f"; echo ;;
 				*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE"; echo ;;
